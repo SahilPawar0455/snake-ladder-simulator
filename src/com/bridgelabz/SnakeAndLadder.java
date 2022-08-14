00dadad349443633a896362d93cdc46ba62d5d5c
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-14 14:08:20 +0530

    ADDED: Player rolls the diee to get a number between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator");
         int startingPosition = 0;
-        System.out.println("Your Starting Position is "+startingPosition);
+        System.out.println("Your Starting Position is " + startingPosition);
+        int die = (int) (Math.random() * (6) + 1);
+        System.out.println("Roll The Die to Get a Number is = " + die);
     }
 }
