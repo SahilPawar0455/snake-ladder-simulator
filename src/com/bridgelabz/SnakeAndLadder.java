0acc9dc000519d5f2461e2a1d9454e789d02d110
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 18:59:03 +0530

    ADDED: The Player then checks for a Option. They are No Play,ladder or snake

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,7 +5,20 @@ public class SnakeAndLadder {
         System.out.println("Welcome to Snake and Ladder Simulator");
         int startingPosition = 0;
         System.out.println("Your Starting Position is " + startingPosition);
+        int nowYourPosition = 0;
         int die = (int) (Math.random() * (6) + 1);
         System.out.println("Roll The Die to Get a Number is = " + die);
+        int option = (int) (Math.random() * (3) + 1);
+        if (option == 1){
+            System.out.println("Your get Ladder");
+            nowYourPosition = nowYourPosition + die;
+        } else if (option == 2) {
+            System.out.println("Your get snake");
+            nowYourPosition = 0;
+        }else {
+            System.out.println("No play");
+            nowYourPosition = nowYourPosition;
+        }
+        System.out.println("Now your Position is "+nowYourPosition);
     }
 }
