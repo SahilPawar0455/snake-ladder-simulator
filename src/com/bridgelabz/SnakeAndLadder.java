3be0a3a6f9bfa049e7843249500f4980f1213c08
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 19:32:19 +0530

    ADDED: Report the Number of Times the Dice was Played

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,10 +6,12 @@ public class SnakeAndLadder {
         int startingPosition = 0;
         System.out.println("Your Starting Position is " + startingPosition);
         int nowYourPosition = 0;
+        int count = 0;
         while (nowYourPosition < 100) {
             int die = (int) (Math.random() * (6) + 1);
             System.out.println("Roll The Die to Get a Number is = " + die);
             int option = (int) (Math.random() * (3) + 1);
+            count++;
             if ((option == 1) && ((nowYourPosition + die) <= 100)) {
                 System.out.println("Your get Ladder");
                 nowYourPosition = nowYourPosition + die;
@@ -23,7 +25,7 @@ public class SnakeAndLadder {
                 System.out.println("No play");
                 nowYourPosition = nowYourPosition;
             }
-            System.out.println("Now your Position is " + nowYourPosition );
+            System.out.println("Now your Position is " + nowYourPosition +" Number of Time Dice are Played "+count);
         }
         System.out.println("You have Win The Game");
     }
