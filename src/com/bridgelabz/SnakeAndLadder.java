586b24b280a363de13da34ef4348a0815e5c50e7
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-16 19:09:16 +0530

    ADDED: Repeat till the Player reaches the winning position 100

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -6,19 +6,24 @@ public class SnakeAndLadder {
         int startingPosition = 0;
         System.out.println("Your Starting Position is " + startingPosition);
         int nowYourPosition = 0;
-        int die = (int) (Math.random() * (6) + 1);
-        System.out.println("Roll The Die to Get a Number is = " + die);
-        int option = (int) (Math.random() * (3) + 1);
-        if (option == 1){
-            System.out.println("Your get Ladder");
-            nowYourPosition = nowYourPosition + die;
-        } else if (option == 2) {
-            System.out.println("Your get snake");
-            nowYourPosition = 0;
-        }else {
-            System.out.println("No play");
-            nowYourPosition = nowYourPosition;
+        while (nowYourPosition < 100) {
+            int die = (int) (Math.random() * (6) + 1);
+            System.out.println("Roll The Die to Get a Number is = " + die);
+            int option = (int) (Math.random() * (3) + 1);
+            if (option == 1) {
+                System.out.println("Your get Ladder");
+                nowYourPosition = nowYourPosition + die;
+            } else if (option == 2) {
+                System.out.println("Your get snake");
+                nowYourPosition = nowYourPosition - die;
+                if (nowYourPosition < 0){
+                    nowYourPosition = 0;
+                }
+            } else {
+                System.out.println("No play");
+                nowYourPosition = nowYourPosition;
+            }
+            System.out.println("Now your Position is " + nowYourPosition);
         }
-        System.out.println("Now your Position is "+nowYourPosition);
     }
 }
